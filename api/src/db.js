6ba5341ef30ce758164f5219eb78ca812fe24e77
MaxMdr2022 <maxi_meder94@hotmail.com>
2022-11-28 18:34:48 -0300

    back

--- api/src/db.js
+++ api/src/db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,DB_NAME
+  DB_USER, DB_PASSWORD, DB_HOST,DB_NAME, DB_URI
 } = process.env;
 
 //--------------deploy---------------------------------------
@@ -50,11 +50,14 @@ const {
 
 //----------------------------------------------------------
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pifood`, {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-});
+// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/ ${DB_NAME}`, {
+//   logging: false, // set to console.log to see the raw SQL queries
+//   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+// });
 
+//------------------------------------------------------------------------------------
+
+const sequelize = new Sequelize(DB_URI, {logging: false, native: false});
 
 const basename = path.basename(__filename);
 
