729dc98ee5405d23a74c1729ae7de48954b85da2
MaxMdr2022 <maxi_meder94@hotmail.com>
2022-11-28 21:36:33 -0300

    db

--- api/src/db.js
+++ api/src/db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,DB_NAME, DB_URI
+  DB_USER, DB_PASSWORD, DB_HOST,DB_NAME,
 } = process.env;
 
 //--------------deploy---------------------------------------
@@ -50,14 +50,13 @@ const {
 
 //----------------------------------------------------------
 
-// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/ ${DB_NAME}`, {
-//   logging: false, // set to console.log to see the raw SQL queries
-//   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-// });
+const sequelize = new Sequelize(`postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+});
 
 //------------------------------------------------------------------------------------
 
-const sequelize = new Sequelize(DB_URI, {logging: false, native: false});
 
 const basename = path.basename(__filename);
 
